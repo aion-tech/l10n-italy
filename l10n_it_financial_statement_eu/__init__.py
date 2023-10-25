a655ee24e5daec135d313aed92484bfcbfd2f3d9
TennyMKY <andrea.martinelli@mkt.it>
2023-10-25 14:42:59 +0200

    inizio script migrazione e correzione hooks

--- l10n_it_financial_statement_eu/__init__.py
+++ l10n_it_financial_statement_eu/__init__.py
@@ -3,8 +3,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
-from . import wizard
 from . import report
+from . import wizard
+# from .hooks import pre_absorb_old_module
 
 from odoo import api, SUPERUSER_ID
 
