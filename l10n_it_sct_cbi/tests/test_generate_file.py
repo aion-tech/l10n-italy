ab7b1383bd5b7d8bc0d28d04dbca3a768130bc39
Simone Rubino <simone.rubino@aion-tech.it>
2024-04-24 11:20:02 +0200

    [MIG] l10n_it_sepa_bonifici: Migration to 16.0 Use new standards Added namespace to pain descendants IBAN is mandatory Initiating party is mandatory FinInstd/Othr is not allowed ReqdExctnDt has new structure BICFI in PaymentRequest instead of BIC Manage multiple priorities Added test Updated README Updated translations
    (cherry picked from commit f22405a446ead2ec0e4510e7ba237dcb73b5e8ff)

--- l10n_it_sct_cbi/tests/test_generate_file.py
+++ l10n_it_sct_cbi/tests/test_generate_file.py
@@ -11,6 +11,8 @@ from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 @tagged("post_install", "-at_install")
 class TestGenerateFile(AccountTestInvoicingCommon):
+    """Test payment file generation."""
+
     @classmethod
     def setUpClass(cls, chart_template_ref=None):
         super().setUpClass(chart_template_ref=chart_template_ref)
@@ -27,6 +29,13 @@ class TestGenerateFile(AccountTestInvoicingCommon):
         cls.bank_journal = cls.company_data["default_journal_bank"]
         cls.bank_journal.bank_account_id = cls.company_bank_account
 
+        cls.supplier_bank = cls.env["res.bank"].create(
+            {
+                "name": "Test supplier bank",
+                "bic": "TESTBICA",
+            }
+        )
+
         cls.supplier = cls.env["res.partner"].create(
             {
                 "name": "Test supplier",
@@ -34,6 +43,7 @@ class TestGenerateFile(AccountTestInvoicingCommon):
                     Command.create(
                         {
                             "acc_number": "IT48N0300203280543765183341",
+                            "bank_id": cls.supplier_bank.id,
                         }
                     ),
                 ],
@@ -64,8 +74,9 @@ class TestGenerateFile(AccountTestInvoicingCommon):
         payment_file_action = payment_order.open2generated()
         return self._get_record_from_action(payment_file_action)
 
-    def test_generate_2_bills(self):
+    def test_2_bills(self):
         """Generate a payment file for 2 vendor bills."""
+        # Arrange
         payment_mode = self.payment_mode
         supplier = self.supplier
 
@@ -90,7 +101,47 @@ class TestGenerateFile(AccountTestInvoicingCommon):
         bills = bill_1 | bill_2
         bills.payment_mode_id = payment_mode
 
+        # Act
         payment_att = self._get_payment_attachment(bills)
+
+        # Assert
         payment_att_content = base64.b64decode(payment_att.datas).decode()
         self.assertIn(bill_1.ref, payment_att_content)
         self.assertIn(bill_2.ref, payment_att_content)
+
+    def test_multiple_payment_priority(self):
+        """Generate a payment file for a vendor bill
+        and use different priority in payments."""
+        # Arrange
+        payment_term = self.env.ref("account.account_payment_term_advance_60days")
+        payment_mode = self.payment_mode
+        supplier = self.supplier
+        bill = self.init_invoice(
+            "in_invoice",
+            partner=supplier,
+            amounts=[
+                100,
+            ],
+            post=False,
+        )
+        with Form(bill) as bill_form:
+            bill_form.ref = "Test multiple payment term"
+            bill_form.invoice_payment_term_id = payment_term
+            bill_form.payment_mode_id = payment_mode
+        bill.action_post()
+
+        payment_order = self._get_payment_order(bill)
+        payment_lines = payment_order.payment_line_ids
+        # pre-condition
+        self.assertEqual(len(payment_lines), 2)
+        payment_lines[0].priority = "HIGH"
+        payment_lines[1].priority = "NORM"
+
+        # Act
+        payment_order.draft2open()
+        payment_file_action = payment_order.open2generated()
+        payment_att = self._get_record_from_action(payment_file_action)
+
+        # Assert
+        payment_att_content = base64.b64decode(payment_att.datas).decode()
+        self.assertIn(bill.ref, payment_att_content)
