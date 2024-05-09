8ff1e8b8b64de0e84bd5088edde30884c261c224
gabriele-baldessari <gabriele.baldessari@gmail.com>
2024-05-09 16:27:51 +0200

    [IMP] ait_cashflow_forecast_account: out_move cashflow records: use date due
    (cherry picked from commit 253b15e15f799b6f3c815cf0f2d1f095e4f7b67b)

--- ait_cashflow_forecast_account/__manifest__.py
+++ ait_cashflow_forecast_account/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "AIT - Cashflow Forecast - Account",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "category": "Accounting",
     "summary": "AIT - Cashflow Forecast - Account",
     "author": "Aion Tech",
