92702ae2f55b36961563afec19e308dbf6013711
Simone Rubino <simone.rubino@aion-tech.it>
2024-09-04 16:12:24 +0200

    [IMP] account_vat_period_end_statement: Journal groups
    (cherry picked from commit 0f4e78cb0b45505cbadcc61454072d4042f7d6c7)

--- account_vat_period_end_statement/__manifest__.py
+++ account_vat_period_end_statement/__manifest__.py
@@ -3,6 +3,7 @@
 #  Copyright 2012-15 LinkIt Spa (<http://http://www.linkgroup.it>)
 #  Copyright 2015 Associazione Odoo Italia (<http://www.odoo-italia.org>)
 #  Copyright 2021 Gianmarco Conte - Dinamiche Aziendali Srl (<www.dinamicheaziendali.it>)
+#  Copyright 2024 Simone Rubino - Aion Tech
 #  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
@@ -15,6 +16,7 @@
     "website": "https://github.com/OCA/l10n-italy",
     "depends": [
         "account",
+        "account_journal_group_included",
         "account_tax_balance",
         "date_range",
         "l10n_it_account",
