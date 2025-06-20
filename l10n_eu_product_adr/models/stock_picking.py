6759f60a414c166dbcd16be5c625e7a505ce1ea2
Guidomadormo <68272608+Guidomadormo@users.noreply.github.com>
2025-06-20 16:04:23 +0200

    [ADD][l10n_it_product_adr]

---
+++ l10n_eu_product_adr/models/stock_picking.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Camptocamp SA
+# Copyright 2021 Opener B.V.
+# Copyright 2024 AionTech
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    adr_points = fields.Float(
+        compute="_compute_adr_points",
+        string="ADR Points",
+        digits="Product Unit of Measure",
+    )
+
+    @api.depends("move_ids.product_id", "move_ids.product_uom_qty")
+    def _compute_adr_points(self):
+        for picking in self:
+            picking.adr_points = sum(sm.adr_points for sm in picking.move_ids)
